7c4c0213d8b830128cdac07651219df9ba79d3b7
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-06-28 17:08:02 +0200

    Add appendMoves function
    The function append all the legal moves to the array
    For each square, check all 8 possibilities and only push the valid ones

--- legalmoves.js
+++ legalmoves.js
@@ -1,1 +1,26 @@
 const legalMoves = [];
+
+appendMoves(legalMoves);
+
+function appendMoves(array) {
+  for (let i = 0; i < 8; i++) {
+    const colMoves = [];
+    for (let j = 0; j < 8; j++) {
+      const squareMoves = [];
+      const col = i;
+      const row = j;
+      checkMove(squareMoves, col - 2, row + 1);
+      checkMove(squareMoves, col - 1, row + 2);
+      checkMove(squareMoves, col + 1, row + 2);
+      checkMove(squareMoves, col + 2, row + 1);
+      checkMove(squareMoves, col + 2, row - 1);
+      checkMove(squareMoves, col + 1, row - 2);
+      checkMove(squareMoves, col - 1, row - 2);
+      checkMove(squareMoves, col - 2, row - 1);
+      colMoves.push(squareMoves);
+    }
+    legalMoves.push(colMoves);
+  }
+}
+
+export { legalMoves };
