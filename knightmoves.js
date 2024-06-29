71996b9929a518de7b675e58aa8fdf29e74f3ba4
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-06-29 12:31:36 +0200

    Add search function
    The function takes 4 + 1 arguments
    col and row, coordinates of the actual square
    path, the actual path (empty on first call)
    faster, which is the actual length of the fastest path
    The function is called recursively for each possible square

--- knightmoves.js
+++ knightmoves.js
@@ -6,3 +6,50 @@ export function knightMoves(startSquare, endSquare) {
   const path = [];
   return search(col, row, endSquare, path);
 }
+
+function search(col, row, target, path, faster = Infinity) {
+  if (path.length >= faster) {
+    return;
+  }
+  if (path.some((square) => square[0] === col && square[1] === row)) {
+    return;
+  }
+  let actualPath = [];
+  path.forEach((square) => {
+    actualPath.push(square);
+  });
+  actualPath.push([col, row]);
+  const nextSquares = legalMoves[col][row];
+  if (
+    nextSquares.some(
+      (square) => square[0] === target[0] && square[1] === target[1]
+    )
+  ) {
+    actualPath.push(target);
+    return actualPath;
+  }
+  const paths = [];
+  let tempFaster = faster;
+  nextSquares.forEach((square) => {
+    const possiblePath = search(
+      square[0],
+      square[1],
+      target,
+      actualPath,
+      tempFaster
+    );
+    if (possiblePath) {
+      paths.push(possiblePath);
+      if (possiblePath.length < tempFaster) tempFaster = possiblePath.length;
+    }
+  });
+  let fastestPath;
+  let fastestPathLength = Infinity;
+  paths.forEach((validPath) => {
+    if (validPath.length < fastestPathLength) {
+      fastestPath = validPath;
+      fastestPathLength = validPath.length;
+    }
+  });
+  return fastestPath;
+}
